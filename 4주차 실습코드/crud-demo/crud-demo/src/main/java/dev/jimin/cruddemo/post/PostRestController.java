2fa4fbf331f2f0222a90193100c1847f9d04c7e8
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2022-02-22 21:23:10 +0900

    update : CRUD & Restful code

--- 4주차 실습코드/crud-demo/crud-demo/src/main/java/dev/jimin/cruddemo/post/PostRestController.java
+++ 4주차 실습코드/crud-demo/crud-demo/src/main/java/dev/jimin/cruddemo/post/PostRestController.java
@@ -2,44 +2,56 @@ package dev.jimin.cruddemo.post;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
-@ResponseBody
-//@RequestMapping("post")
-public class PostController {
-    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
+@RestController
+@RequestMapping("post")
+public class PostRestController {
+    private static final Logger logger = LoggerFactory.getLogger(PostRestController.class);
     private final List<PostDto> postList;
 
-    public PostController() {
-        postList = new ArrayList<>();
+    public PostRestController() {
+        this.postList = new ArrayList<>();
     }
 
-    @PostMapping("create")
+    // http://localhost:8080/post
+    // POST /post
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
     public void createPost(@RequestBody PostDto postDto) {
         logger.info(postDto.toString());
         this.postList.add(postDto);
     }
 
-    @GetMapping("read-all")
+    // http://localhost:8080/post
+    // GET /post
+
+    @GetMapping()
     public List<PostDto> readPostAll() {
-        logger.info("in read all");
+        logger.info("in read post all");
         return this.postList;
     }
 
-    @GetMapping("read-one")
-    public PostDto readPostOne(@RequestParam("id") int id) {
-        logger.info("in read one");
+    // GET /post/0
+    // GET /post?id=0
+
+    @GetMapping("{id}")
+    public PostDto readPost(@PathVariable("id") int id) {
+        logger.info("in read post");
         return this.postList.get(id);
     }
 
-    @PostMapping("update")
+    // PUT /post/0
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public void updatePost(
-            @RequestParam("id") int id,
+            @PathVariable("id") int id,
             @RequestBody PostDto postDto
     ){
         PostDto targetPost = this.postList.get(id);
@@ -52,8 +64,11 @@ public class PostController {
         this.postList.set(id, targetPost);
     }
 
-    @DeleteMapping("delete")
-    public void deletePost(@RequestParam("id") int id) {
+    // DELETE /post/0
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deletePost(@PathVariable("id") int id) {
         this.postList.remove(id);
     }
 }
