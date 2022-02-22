6ddbd09e725fc5c808e3a0ba4a02831a45850c21
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2022-02-22 17:13:07 +0900

    add : Postman + PostMapping

--- 2차 Mission/controller-demo/controller-demo/src/main/java/dev/jiminyoo/controllerdemo/SampleRestController.java
+++ 2차 Mission/controller-demo/controller-demo/src/main/java/dev/jiminyoo/controllerdemo/SampleRestController.java
@@ -3,10 +3,10 @@ package dev.jiminyoo.controllerdemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.awt.*;
 import java.io.FileInputStream;
@@ -18,9 +18,32 @@ import java.io.InputStream;
 public class SampleRestController {
     private static final Logger logger = LoggerFactory.getLogger(SampleRestController.class);
 
-    @GetMapping("sample-playload")
-    public SamplePlayload samplePlayload(){
+    @GetMapping("/sample-playload")
+    public SamplePlayload samplePayloadGet(){
         return new SamplePlayload("jimin", 23, "developer");
+    }
+
+    @PostMapping("/sample-playload")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void samplePayloadPost(@RequestBody SamplePlayload samplePlayload) {
+        logger.info(samplePlayload.toString());
+    }
+
+    @PostMapping(
+            value = "/sample-multipart",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void sampleMultipartPost(
+            @RequestParam("name") String name,
+            @RequestParam("age") Integer age,
+            @RequestParam("occupation") String occupation,
+            @RequestParam("file") MultipartFile multipartFile
+    ){
+        logger.info("name: " + name);
+        logger.info("age: " + age);
+        logger.info("occupation: " + occupation);
+        logger.info("file original name: " + multipartFile.getOriginalFilename());
     }
 
     @GetMapping(
