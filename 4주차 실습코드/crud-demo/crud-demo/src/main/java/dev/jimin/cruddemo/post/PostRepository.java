571adc38a9dd2ddc8994d378408020a367e3ee57
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2022-02-22 22:43:53 +0900

    update : CRUD - Spring Stereotypes

---
+++ 4주차 실습코드/crud-demo/crud-demo/src/main/java/dev/jimin/cruddemo/post/PostRepository.java
@@ -0,0 +1,11 @@
+package dev.jimin.cruddemo.post;
+
+import java.util.List;
+
+public interface PostRepository {
+    boolean save(PostDto dto);
+    List<PostDto> findAll();
+    PostDto findById(int id);
+    boolean update(int id, PostDto dto);
+    boolean delete(int id);
+}
--- 4주차 실습코드/crud-demo/crud-demo/src/main/java/dev/jimin/cruddemo/post/PostRestController.java
+++ 4주차 실습코드/crud-demo/crud-demo/src/main/java/dev/jimin/cruddemo/post/PostRestController.java
@@ -2,9 +2,11 @@ package dev.jimin.cruddemo.post;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,10 +14,10 @@ import java.util.List;
 @RequestMapping("post")
 public class PostRestController {
     private static final Logger logger = LoggerFactory.getLogger(PostRestController.class);
-    private final List<PostDto> postList;
+    private final PostService postService;
 
-    public PostRestController() {
-        this.postList = new ArrayList<>();
+    public PostRestController(@Autowired PostService postService) {
+        this.postService = postService;
     }
 
     // http://localhost:8080/post
@@ -25,7 +27,7 @@ public class PostRestController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createPost(@RequestBody PostDto postDto) {
         logger.info(postDto.toString());
-        this.postList.add(postDto);
+        this.postService.createPost(postDto);
     }
 
     // http://localhost:8080/post
@@ -34,7 +36,7 @@ public class PostRestController {
     @GetMapping()
     public List<PostDto> readPostAll() {
         logger.info("in read post all");
-        return this.postList;
+        return this.postService.readPostAll();
     }
 
     // GET /post/0
@@ -43,25 +45,19 @@ public class PostRestController {
     @GetMapping("{id}")
     public PostDto readPost(@PathVariable("id") int id) {
         logger.info("in read post");
-        return this.postList.get(id);
+        return this.postService.readPost(id);
     }
 
     // PUT /post/0
 
-    @PutMapping("{id}")
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping("update")
     public void updatePost(
-            @PathVariable("id") int id,
+            @RequestParam("id") int id,
             @RequestBody PostDto postDto
     ){
-        PostDto targetPost = this.postList.get(id);
-        if (postDto.getTitle() != null) {
-            targetPost.setTitle(postDto.getTitle());
-        }
-        if (postDto.getContent() != null) {
-            targetPost.setContent(postDto.getContent());
-        }
-        this.postList.set(id, targetPost);
+        logger.info("target id: " + id);
+        logger.info("update content" + postDto);
+        this.postService.updatePost(id, postDto);
     }
 
     // DELETE /post/0
@@ -69,6 +65,6 @@ public class PostRestController {
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void deletePost(@PathVariable("id") int id) {
-        this.postList.remove(id);
+        this.postService.deletePost(id);
     }
 }
