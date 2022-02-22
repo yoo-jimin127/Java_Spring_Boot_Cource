39cc2c714ed424217e911d6e800dd4f004f7f8ec
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2022-02-22 15:16:15 +0900

    add : Controller와 Restcontroller

--- 2차 Mission/controller-demo/controller-demo/src/main/java/dev/jiminyoo/controllerdemo/SampleController.java
+++ 2차 Mission/controller-demo/controller-demo/src/main/java/dev/jiminyoo/controllerdemo/SampleController.java
@@ -3,19 +3,36 @@ package dev.jiminyoo.controllerdemo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SampleController {
     private static final Logger logger
             = LoggerFactory.getLogger(SampleController.class);
 
-    @RequestMapping(
-            value = "hello",
-            method = RequestMethod.GET
+    @GetMapping(
+            value = "/hello"
     )
-    public String hello() {
-        return "hello.html";
+    public String hello(@RequestParam(
+            name = "id", required = false, defaultValue = "") String id
+    ) {
+        logger.info("Path: hello");
+        logger.info("Query Param id: " + id);
+        return "/hello.html";
+    }
+
+    @GetMapping(
+            value="/hello/{id}"
+    )
+    public String helloPath(@PathVariable("id") String id) {
+        logger.info("Path Variable is: " + id);
+        return "/hello.html";
+    }
+
+    @GetMapping(
+            "/get-profile"
+    )
+    public @ResponseBody SamplePlayload getProfile() {
+        return new SamplePlayload("jimin", 23, "developer");
     }
 }
