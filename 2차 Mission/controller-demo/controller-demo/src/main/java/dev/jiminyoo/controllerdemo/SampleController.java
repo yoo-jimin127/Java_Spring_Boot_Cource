a1f4745c6a95f39c9d12a0f27cca2ce8365e7be5
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2022-02-22 16:34:06 +0900

    add : HTML template - thymeleaf & JSP

--- 2차 Mission/controller-demo/controller-demo/src/main/java/dev/jiminyoo/controllerdemo/SampleController.java
+++ 2차 Mission/controller-demo/controller-demo/src/main/java/dev/jiminyoo/controllerdemo/SampleController.java
@@ -3,36 +3,44 @@ package dev.jiminyoo.controllerdemo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequestMapping("view")
 public class SampleController {
     private static final Logger logger
             = LoggerFactory.getLogger(SampleController.class);
 
-    @GetMapping(
-            value = "/hello"
-    )
-    public String hello(@RequestParam(
-            name = "id", required = false, defaultValue = "") String id
-    ) {
-        logger.info("Path: hello");
-        logger.info("Query Param id: " + id);
-        return "/hello.html";
+    @GetMapping("/sample-jsp")
+    public String sampleJsp(Model model)  {
+        logger.info("in sample jsp");
+        List<SamplePlayload> profiles = new ArrayList<>();
+        profiles.add(new SamplePlayload("Adam", 22, "Student"));
+        profiles.add(new SamplePlayload("Bradley", 29, "Accountant"));
+        profiles.add(new SamplePlayload("Adam", 35, "Developer"));
+
+        model.addAttribute("profiles", profiles);
+
+        return "view-jsp";
     }
 
-    @GetMapping(
-            value="/hello/{id}"
-    )
-    public String helloPath(@PathVariable("id") String id) {
-        logger.info("Path Variable is: " + id);
-        return "/hello.html";
+    @GetMapping("/sample-thyme")
+    public ModelAndView sampleThyme() {
+        logger.info("in sample thyme");
+        ModelAndView modelAndView = new ModelAndView();
+        List<SamplePlayload> profiles = new ArrayList<>();
+        profiles.add(new SamplePlayload("Adam", 22, "Student"));
+        profiles.add(new SamplePlayload("Bradley", 29, "Accountant"));
+        profiles.add(new SamplePlayload("Adam", 35, "Developer"));
+
+        modelAndView.addObject("profiles", profiles);
+        modelAndView.setViewName("view-thyme");
+        return modelAndView;
     }
 
-    @GetMapping(
-            "/get-profile"
-    )
-    public @ResponseBody SamplePlayload getProfile() {
-        return new SamplePlayload("jimin", 23, "developer");
-    }
 }
