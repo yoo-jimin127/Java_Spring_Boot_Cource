536037368fbbbf3cb1738f581e1d475fe94df76d
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2022-03-08 18:28:17 +0900

    update : apply database at CRUD

--- 5주차 실습코드/jpa/jpa/src/main/java/dev/jimin/jpa/repository/PostRepository.java
+++ 5주차 실습코드/jpa/jpa/src/main/java/dev/jimin/jpa/repository/PostRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface PostRepository extends CrudRepository<PostEntity, Long> {
-    List<PostEntity> findAllByWriter(String writer);  // where writer = ?
-    List<PostEntity> findAllByWriterAndBoardEntity(String writer, BoardEntity boardEntity); // where writer = ? and board_entity_id = ?
-    List<PostEntity> findAllByWriterContaining(String writer);
+//    List<PostEntity> findAllByWriter(String writer);  // where writer = ?
+//    List<PostEntity> findAllByWriterAndBoardEntity(String writer, BoardEntity boardEntity); // where writer = ? and board_entity_id = ?
+//    List<PostEntity> findAllByWriterContaining(String writer);
 }
